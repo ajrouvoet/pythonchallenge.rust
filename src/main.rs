dcf26e8ce92a6e3ae07e240710c52ee0a17bd624
Arjen Rouvoet <a.j.rouvoet@gmail.com>
2018-08-05 16:32:51 +0200

    Solved 2

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use num::pow::pow;
 struct Challenge {
     num: u8,
     url: String,
+    hints: Vec<String>,
 
     solution_url: String
 }
@@ -14,7 +15,11 @@ impl ToString for Challenge {
   fn to_string(&self) -> String {
       let title = format!(" Challenge {} ", self.num);
       let bar = String::from(format!("{:-^1$}\n", title, 80));
-      return bar + &format!("from: {}\n", self.url) + &format!("to: {}\n", self.solution_url);
+      let hints: String = self.hints.iter().map(|h| format!("  - {}\n", h)).collect();
+      return bar
+          + &format!("from: {}\n", self.url)
+          + &format!("to: {}\n", self.solution_url)
+          + &format!("via: \n{}", hints);
   }
 }
 
@@ -25,15 +30,36 @@ fn challenge_01() -> Challenge {
     Challenge {
         num: 1,
         url: String::from("http://www.pythonchallenge.com/pc/def/0.html"),
+        hints: Vec::new(),
         solution_url: format!("http://www.pythonchallenge.com/pc/def/{}.html", i)
     }
 }
 
 fn challenge_02() -> Challenge {
+    let cipher:&[u8] =
+        b"g fmnc wms bgblr rpylqjyrc gr zw fylb. \
+         rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr \
+         ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq \
+         pcamkkclbcb. lmu ynnjw ml rfc spj.";
+
+
+    fn decipher(cipher: &[u8], offset: u8) -> String {
+        let decode = | &c | (if c > 96 {(c - 97 + offset) % 26 + 97} else { c });
+        return String
+            ::from_utf8(
+              cipher
+                  .iter()
+                  .map(decode)
+                  .collect()
+            )
+            .expect("Could not decode plain text utf8");
+    }
+
     Challenge {
         num: 2,
         url: String::from("http://www.pythonchallenge.com/pc/def/map.html"),
-        solution_url: format!("")
+        hints: vec![decipher(cipher, 2)],
+        solution_url: format!("http://www.pythonchallenge.com/pc/def/{}.html", decipher(b"map", 2))
     }
 }
 
